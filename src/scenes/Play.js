4e58079452ea3365bf627ec011c042983e6d6f34
“Purisen” <gmarcelsmith@gmail.com>
2022-04-08 15:16:09 -0700

    i forgor :skull:

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -48,6 +48,33 @@ class Play extends Phaser.Scene {
             key: 'explode',
             frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}), frameRate: 30
         });
+
+        // initialize score
+        this.p1Score = 0;
+          // display score
+        let scoreConfig = {
+        fontFamily: 'Courier',
+        fontSize: '28px',
+        backgroundColor: '#F3B141',
+        color: '#843605',
+        align: 'right',
+        padding: {
+            top: 5,
+            bottom: 5,
+        },
+            fixedWidth: 100
+        }
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+        
+        // GAME OVER flag
+        this.gameOver = false;
+
+        // 60-second play clock
+        scoreConfig.fixedWidth = 0;
+        this.clock = this.time.delayedCall(1000, () => {
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+        }, null, this);
     }
 
     update() {
@@ -95,6 +122,9 @@ class Play extends Phaser.Scene {
             ship.alpha = 1;                         // make ship visible again
             boom.destroy();                         // remove explosion sprite
         });
+        // score add and repaint
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score;
     }
 
 }
