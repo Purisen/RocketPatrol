c1bc7cfaf9157aeb61b35e9c4e9003fdf5289516
“Purisen” <gmarcelsmith@gmail.com>
2022-04-07 19:10:31 -0700

    collisions now reset

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -18,4 +18,9 @@ class Spaceship extends Phaser.GameObjects.Sprite {
             this.x = game.config.width;
         }
     }
+
+    // position reset
+    reset() {
+        this.x = game.config.width;
+    }
 }
